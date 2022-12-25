25797f581b0a64de48e2ba17b387f08c233e72a1
Peter Luladjiev <luladjiev@gmail.com>
2022-12-25 12:01:06 +0200

    Enable netrw

--- lua/luladjiev/init.lua
+++ lua/luladjiev/init.lua
@@ -106,7 +106,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-
--- disable netrw
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
